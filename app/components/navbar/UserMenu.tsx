b68798afbeb69d2a17f797a9cc36987a93cbeada
Raj Bagaria <bagariaraj23@gmail.com>
2023-04-14 16:14:07 +0530

    login functionality

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -5,9 +5,17 @@ import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 import MenuItem from "./MenuItem";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
+import useLoginModal from "@/app/hooks/useLoginModal";
+import { User } from "@prisma/client";
+import { signOut } from "next-auth/react";
 
-const UserMenu = () => {
-  const registerModal= useRegisterModal();
+interface UserMenuProps {
+  currentUser?: User | null;
+}
+
+const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
   const [isOpen, setIsOpen] = useState(false);
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
@@ -35,16 +43,22 @@ const UserMenu = () => {
       {isOpen && (
         <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
           <div className="flex flex-col cursor-pointer">
-            <>
-            <MenuItem 
-                onClick={()=>{}}
-                label="login"
-            />
-            <MenuItem 
-                onClick={(registerModal.onOpen)}
-                label="sign up"
-            />
-            </>
+            {currentUser ? (
+              <>
+                <MenuItem onClick={() => {}} label="My Trips" />
+                <MenuItem onClick={() => {}} label="My Reservations" />
+                <MenuItem onClick={() => {}} label="My Favorites" />
+                <MenuItem onClick={() => {}} label="My Vehicles" />
+                <MenuItem onClick={() => {}} label="Cloud-BnC My Vehicle" />
+                <hr />
+                <MenuItem onClick={() => signOut()} label="Logout" />
+              </>
+            ) : (
+              <>
+                <MenuItem onClick={loginModal.onOpen} label="login" />
+                <MenuItem onClick={registerModal.onOpen} label="sign up" />
+              </>
+            )}
           </div>
         </div>
       )}
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import RegisterModal from "./components/modals/RegisterModal";
 import ToasterProvider from "./providers/ToasterProvider";
+import LoginModal from "./components/modals/LoginModal";
+import { getCurrentUser } from "./actions/getCurrentUser";
 
 export const metadata = {
   title: "Cloud-BnC",
@@ -14,18 +16,22 @@ const font = Nunito({
   subsets: ["latin"],
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
+  const currentUser = await getCurrentUser();
+  
   return (
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
           <RegisterModal />
-          <Navbar />
+          <LoginModal />
+          <Navbar currentUser = {currentUser}/>
         </ClientOnly>
         {children}
       </body>
