a6e20e7d9e218ba751075d4db0a2966b317f760c
Raj Bagaria <bagariaraj23@gmail.com>
2023-04-24 22:50:34 +0530

    lint

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -74,7 +74,7 @@ const RentModal = () => {
       dynamic(() => import("../Map"), {
         ssr: false,
       }),
-    [location]
+    []
   );
 
   const setCustomValue = (id: string, value: any) => {
