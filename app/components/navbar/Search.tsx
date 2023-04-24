552a80631c6db8c04731c10279659479d21c4ead
Raj Bagaria <bagariaraj23@gmail.com>
2023-04-24 22:06:27 +0530

    loader and errors

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,24 +1,67 @@
 "use client";
 
+import useLocation from "@/app/hooks/useLocation";
 import useSearchModal from "@/app/hooks/useSearchModal";
+import { differenceInDays } from "date-fns";
+import { useSearchParams } from "next/navigation";
+import { useMemo } from "react";
 import { BiSearch } from "react-icons/bi";
 
 const Search = () => {
-
   const searchModal = useSearchModal();
+  const params = useSearchParams();
+  const { getByValue } = useLocation();
 
+  const locationValue = params?.get("location");
+  const startDate = params?.get("startDate");
+  const endDate = params?.get("endDate");
+  const passengerCount = params?.get("passengerCount");
+
+  const locationLabel = useMemo(() => {
+    if (locationValue) {
+      return getByValue(locationValue as string)?.label;
+    }
+
+    return "Anywhere";
+  }, [getByValue, locationValue]);
+ 
+  const durationLabel = useMemo(() => {
+    if (startDate && endDate) {
+      const start = new Date(startDate as string);
+      const end = new Date(endDate as string);
+
+      let diff = differenceInDays(end, start);
+
+      if (diff === 0) {
+        diff = 1;
+      }
+
+      return `${diff} Days`;
+    }
+
+    return "Any Week";
+  }, [startDate, endDate]);
+
+  const passengerLabel = useMemo(() => {
+    if (passengerCount) {
+      return `${passengerCount} Passenger`;
+    }
+
+    return "Add Passengers";
+},[passengerCount]);
 
   return (
     <div
-    onClick={searchModal.onOpen}
-    className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hove:shadow-md transition cursor-pointer">
+      onClick={searchModal.onOpen}
+      className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hove:shadow-md transition cursor-pointer"
+    >
       <div className="flex flex-row items-center justify-between">
-        <div className="text-sm font-semibold px-6">Anywhere</div>
+        <div className="text-sm font-semibold px-6">{locationLabel}</div>
         <div className="hidden sm:block text-sm font-semibold px-6 flex-1 border-x-[1px] text-center">
-          Any Week
+          {durationLabel}
         </div>
         <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
-          <div className="hidden sm:block">Add Renters</div>
+          <div className="hidden sm:block">{passengerLabel}</div>
           <div className="p-2 bg-[#5A5D9D] rounded-full text-white">
             <BiSearch size={18} />
           </div>
